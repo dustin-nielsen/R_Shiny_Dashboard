e3558f4e439c760142cc5ea73c5251166ea0d508
dustin-nielsen <dustinultimate@gmail.com>
2023-04-19 07:42:40 -0600

    done?

--- server.R
+++ server.R
@@ -17,78 +17,134 @@ server <- function(input,output,session) {
     dt <- merge(dt,patent,by = 'patent_id')
     # merge with assignee
     dt <- merge(dt,assignee,by = 'patent_id')
+    # merge location 
+    dt <- merge(dt,location,by = 'location_id')
+
+    # Get top 10 companies
+    totals <- dt %>%
+      filter(disambig_assignee_organization!='') %>%
+      group_by(disambig_assignee_organization) %>%
+      summarize(total=uniqueN(patent_id))
+
+    totals <- totals[order(totals$total,decreasing = T),] %>% slice(1:10)
+
+    # Calculate 5 year CAGR for top 10 companies
+    cagr <- data.frame(expand.grid(year=2017:2021,disambig_assignee_organization=totals$disambig_assignee_organization))
+
+    temp <- dt %>%
+      filter(disambig_assignee_organization %in% totals$disambig_assignee_organization) %>%
+      group_by(year=year(patent_date),disambig_assignee_organization) %>%
+      summarise(n=uniqueN(patent_id))
+    cagr <- merge(cagr,temp,by = c('year','disambig_assignee_organization'),all.x = T)
+    rm(temp)
+    cagr[is.na(cagr)] <- 0
+    cagr <- cagr %>%
+      group_by(disambig_assignee_organization) %>%
+      mutate(cum_cnt = cumsum(n)) %>%  # make sure your date are sorted correctly before calculating the cumulative :)
+      filter(year %in% c(2017,2021)) %>%
+      pivot_wider(id_cols = disambig_assignee_organization,names_from = year,values_from = cum_cnt)
+    cagr$cagr_2017_2021 <- round(((cagr$`2021`/cagr$`2017`)^(1/5))-1,3)
+
+
+    # Calculate avg claim count for top 10 companies
+    claims <- dt %>%
+      filter(disambig_assignee_organization %in% totals$disambig_assignee_organization) %>%
+      select(disambig_assignee_organization,patent_id,num_claims) %>%
+      unique() %>%
+      group_by(disambig_assignee_organization) %>%
+      summarise(avg_claims=round(mean(num_claims)))
+
+    # Combine and save file
+    totals <- merge(totals,cagr,by = 'disambig_assignee_organization')
+    totals <- merge(totals,claims,by = 'disambig_assignee_organization')
+    totals <- totals %>% select(-`2017`,-`2021`)
     
-    competition$dt <- head(dt) #temp head(dt)
+    competition$dt <- totals
     
     output$competition_dt <- renderDT({
       datatable(
-        competition$dt, 
-        #colnames = colnames(competition$dt)[1:6,8,10,13]
-        #select = c("patent_id","cpc_group","patent_date","patent_title","num_claims","disambig_assignee_organization", "disambig_assignee_individual_name_first", "disambig_assignee_individual_name_last")
-        # options = list(
-        #   columnDefs = list(list(targets = c(2, 3, 4, 5, 6, 7), searchable = FALSE))
-        #   )
-        )
-      })
+        competition$dt
+      )
+    })
     
-    # renderDT({
-    #   datatable(data, 
-    #             options = list(
-    #               columnDefs = list(list(targets = c(2, 3), searchable = FALSE))
-    #             )
-    #   )
-    # })
+    # Identify patents in each segment (benefit of doing it this way? scale)
+    # segments_names <- c('Photovoltaic','Wind','Other Renewable Energy Sources', 'Combustion Mitigation', 'Nuclear', 'Electrical Efficiency', 'Non-Fossil Fuel', 'Energy Storage', 'Hydrogen', 'Other GHG Reduction Technology')
+    # segments_codes <- c('Y02E10/5','Y02E10/7','Y02E10/1|Y02E10/2|Y02E10/3|Y02E10/4|Y02E10/6', 'Y02E2', 'Y02E3', 'Y02E4', 'Y02E5', 'Y02E60/1', 'Y02E60/3|Y02E60/5', 'Y02E60/6|Y02E7')
+    segments_names <- unlist(strsplit(input$submarket_labels_input, ","))
+    segments_codes <- unlist(strsplit(input$submarket_cpcs_input, ","))
+    
+    segments_dtlist <- list()
+    for (segment in segments_codes) {
+      segments_dtlist[[length(segments_dtlist)+1]] <- dt[grepl(pattern = segment,x = dt$cpc_group,ignore.case = T),c('patent_id','disambig_assignee_organization')] %>% unique()
+    }
+    names(segments_dtlist) <- segments_names
+    
+    # Assemble data and pivot
+    for (segment in segments_names) {
+      temp <- segments_dtlist[[segment]] %>% group_by(disambig_assignee_organization) %>% summarise(n=uniqueN(patent_id))
+      colnames(temp)[colnames(temp)=='n'] <- segment
+      totals <- merge(totals,temp,by = 'disambig_assignee_organization',all.x = T)
+    }  
+    totals[is.na(totals)] <- 0
+    
+    totals <- totals %>% arrange(desc(total))
+    
+    competition$sub_seg <- totals
+    
+    output$competition_sub_seg <- renderDT({
+      datatable(
+        competition$sub_seg
+      )
+    })
+    
+    # tidy up the location data
+    dt$state_fips <- str_pad(string = dt$state_fips,width = 2,side = 'left',pad = '0')
+    dt$county_fips <- str_pad(string = dt$county_fips,width = 3,side = 'left',pad = '0')
+    dt$fips <- paste(dt$state_fips,dt$county_fips,sep = '')
+
+    # County level
+    # Summarize data by county
+    dt_county <- dt %>%
+      filter(!is.na(county_fips)) %>%
+      group_by(disambig_state,county,fips) %>%
+      summarise(n=uniqueN(patent_id))
+
+    # get geojson files for mapping
+    url <- 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
+    counties <- rjson::fromJSON(file = url)
+
+    g <- list(
+      scope = 'usa',
+      projection = list(type = 'albers usa'),
+      showlakes = TRUE,
+      lakecolor = toRGB('white')
+    )
+
+    fig <- plot_ly()
+    fig <- fig %>% add_trace(
+      type="choropleth",
+      geojson=counties,
+      locations= dt_county$fips,
+      z=dt_county$n,
+      colorscale="Viridis",
+      zmin=0,
+      zmax=3000,
+      marker=list(line=list(
+        width=0)
+      )
+    )
+    fig <- fig %>% colorbar(title = "Number of patents")
+    fig <- fig %>% layout(
+      title = "Patents granted by county",
+      geo = g
+    )
+
+    competition$plot <- fig
+
+    output$competition_plot <- renderPlotly({
+      competition$plot
+    })
   })
-  
-  # Reactivity:
-  # As a general rule, use reactive() when you just want to update something based on a new value (e.g. a user input)
-  # and use reactiveVal or reactiveValues when you have an object that you want to maintain a state
-  
-  # # Reactive objects:
-  # text_labels_input <- reactive({
-  #   req(input$submarket_labels_input)
-  #   paste('You entered: ',input$submarket_labels_input,sep = '',collapse = '')
-  #   })
-  # output$text_labels_output <- renderText({text_labels_input()})
-  # 
-  # # # Reactive objects:
-  # market_cpc_input_text <- reactive({
-  #   req(input$market_cpcs_input)
-  #   paste(input$market_cpcs_input,sep = '',collapse = ', ')
-  #   })
-  # output$market_cpc_output <- renderText({market_cpc_input_text()})
-  # 
-  # 
-  # # Reactive values and observers
-  # r_list <- reactiveValues(txt = 'You entered: ',txt2 = 'You typed: ')
-  # observe({
-  #   output$text_labels_output <- renderText({
-  #     paste(r_list$txt, input$submarket_labels_input,sep = '',collapse = '')
-  #   })
-  #   })
-  # 
-  # 
-  # # ObserveEvent
-  # r_list <- reactiveValues(txt = 'You entered: ',txt2 = 'You typed: ')
-  # observeEvent(input$generate_competitive_positioning,{
-  #   r_list$txt <- paste(r_list$txt, input$submarket_labels_input,sep = '',collapse = '')
-  # })
-  # output$text_labels_output <- renderText({
-  #   r_list$txt
-  # })
-  # 
-  # 
-  # # Debugging - browser()
-  #   r_list <- reactiveValues(txt = 'You entered: ',txt2 = 'You typed: ')
-  #   observeEvent(input$generate_competitive_positioning,{
-  #     browser()
-  #     temp <- 1
-  #     print(temp*5)
-  #     r_list$txt <- paste(r_list$txt, input$submarket_labels_input,sep = '',collapse = '')
-  # 
-  #   })
-  #   output$text_labels_output <- renderText({
-  #     r_list$txt
-  #   })
+
 }
 
--- ui.R
+++ ui.R
@@ -1,328 +1,3 @@
-
-
-# Define UI ----
-# FROM WEBSITE - LIST OF WIDGETS
-#  ui <- fluidPage(
-#   titlePanel("Basic widgets"),
-#  
-#    fluidRow(
-#  
-#     column(3,
-#             h3("Buttons"),
-#            actionButton("action", "Action"),
-#            br(),
-#            br(),
-#            submitButton("Submit")),
-# 
-#     column(3,
-#            h3("Single checkbox"),
-#            checkboxInput("checkbox", "Choice A", value = TRUE)),
-# 
-#     column(3,
-#            checkboxGroupInput("checkGroup",
-#                               h3("Checkbox group"),
-#                               choices = list("Choice 1" = 1,
-#                                              "Choice 2" = 2,
-#                                              "Choice 3" = 3),
-#                               selected = 1)),
-# 
-#     column(3,
-#            dateInput("date",
-#                      h3("Date input"),
-#                      value = "2014-01-01"))
-#   ),
-# 
-#   fluidRow(
-# 
-#     column(3,
-#            dateRangeInput("dates", h3("Date range"))),
-# 
-#     column(3,
-#            fileInput("file", h3("File input"))),
-# 
-#     column(3,
-#            h3("Help text"),
-#            helpText("Note: help text isn't a true widget,",
-#                     "but it provides an easy way to add text to",
-#                     "accompany other widgets.")),
-# 
-#     column(3,
-#            numericInput("num",
-#                         h3("Numeric input"),
-#                         value = 1))
-#   ),
-# 
-#   fluidRow(
-# 
-#     column(3,
-#            radioButtons("radio", h3("Radio buttons"),
-#                         choices = list("Choice 1" = 1, "Choice 2" = 2,
-#                                        "Choice 3" = 3),selected = 1)),
-# 
-#     column(3,
-#            selectInput("select", h3("Select box"),
-#                        choices = list("Choice 1" = 1, "Choice 2" = 2,
-#                                       "Choice 3" = 3), selected = 1)),
-# 
-#     column(3,
-#            sliderInput("slider1", h3("Sliders"),
-#                        min = 0, max = 100, value = 50),
-#            sliderInput("slider2", "",
-#                        min = 0, max = 100, value = c(25, 75))
-#     ),
-# 
-#     column(3,
-#            textInput("text", h3("Text input"),
-#                      value = "Enter text..."))
-#   )
-# )
-# 
-
-################################################################################
-################################################################################
-# Layout
-# fluidpage
-# navbarpage
-# shinydashboard
-
-
-################################################################################
-# fluidpage
-
-# ui <- fluidPage(
-# 
-#   fluidRow(
-#     h1('Page title')
-#   ),
-#   fluidRow(
-#     column(width = 6,
-#            wellPanel(
-#              p('Hello world')
-#            )
-#            ),
-#     column(width = 6,
-#            wellPanel(
-#              p('Panel 2')
-#            )
-#            )
-#   ),
-#   tabsetPanel(
-#     tabPanel(title = 'Inputs',
-#              wellPanel(
-#             textInput(inputId = 'my_input',label = 'Input',width = '200px',placeholder = 'Enter text here')
-#              )),
-#     tabPanel(title = 'Outputs',
-#              wellPanel(
-#                plotlyOutput(outputId = 'my_output')
-#              ))
-#   )
-# 
-# )
-
-
-
-################################################################################
-# navbarpage
-
-# ui <- navbarPage(title = 'Patent Analytics',
-# 
-#   tabPanel(title = 'Home',
-#            titlePanel('Home'),
-#            sidebarLayout(
-#              sidebarPanel = sidebarPanel(width = 3,
-#                                          h4('Side bar')
-#                                          ),
-#              mainPanel = mainPanel(
-#                tags$h4('Main bar')
-#              )
-#            )
-#            ),
-# 
-#   tabPanel(title = 'Competitive positioning',
-#            # fluidPage(
-#            #   fluidRow()
-#            # )
-#            fluidPage(
-# 
-#              fluidRow(
-#                h1('Page title')
-#              ),
-#              fluidRow(
-#                column(width = 6,
-#                       wellPanel(
-#                         p('Hello world')
-#                       )
-#                       ),
-#                column(width = 6,
-#                       wellPanel(
-#                         p('Panel 2')
-#                       )
-#                       )
-#              ),
-#              tabsetPanel(
-#                tabPanel(title = 'Inputs',
-#                         wellPanel(
-#                        textInput(inputId = 'my_input',label = 'Input',width = '200px',placeholder = 'Enter text here')
-#                         )),
-#                tabPanel(title = 'Outputs',
-#                         wellPanel(
-#                           plotlyOutput(outputId = 'my_output')
-#                         ))
-#              )
-# 
-#            )
-# 
-# 
-#            ),
-# 
-#   tabPanel(title = 'Technology trends',
-# 
-# 
-# 
-#            )
-# 
-# )
-
-################################################################################
-# dashboard
-
-# ui <- shinydashboard::dashboardPage(
-# 
-#   # header
-#       dashboardHeader(title = 'Patent analytics'),
-# 
-#   # sidebar
-#       dashboardSidebar(
-#         sidebarMenu(
-#           menuItem(text = 'Page 1',tabName = 'page1'),
-#           menuItem(text = 'Page 2',tabName = 'page2',
-#                    menuSubItem('Page 2.1',tabName = 'page2-1'),
-#                    menuSubItem('Page 2.2',tabName = 'page2-2')
-#                    )
-#         )
-# 
-#       ),
-#   # body
-#       dashboardBody(
-#         tabItems(
-#           tabItem(tabName = 'page1',
-#                   tags$h4('Hello world')
-#           ),
-#           tabItem(tabName = 'page2-1',
-#                   HTML('<p style="font-size:18pt; color:blue;">Hello world</p>'),
-#                   mainPanel(
-#                     fluidPage(
-#                       fluidRow(
-#                         h1('Page title')
-#                       ),
-#                       fluidRow(
-#                         column(width = 6,
-#                                wellPanel(
-#                                  p('Hello world')
-#                                )
-#                         ),
-#                         column(width = 6,
-#                                wellPanel(
-#                                  p('Panel 2')
-#                                )
-#                         )
-#                       ),
-#                       tabsetPanel(
-#                         tabPanel(title = 'Inputs',
-#                                  wellPanel(
-#                                    textInput(inputId = 'my_input',label = 'Input',width = '200px',placeholder = 'Enter text here')
-#                                  )),
-#                         tabPanel(title = 'Outputs',
-#                                  wellPanel(
-#                                    plotlyOutput(outputId = 'my_output')
-#                                  ))
-#                     ))
-# 
-#           ),
-#           tabItem(tabName = 'page2-2',
-#                   HTML('<p style="font-size:24pt; color:red;">Hello world</p>'),
-#                   sidebarPanel(
-#                     h1('this is my sidebar')
-#                   ),
-#                   mainPanel(
-# 
-#                   )
-#           )
-#         )
-# 
-#       )
-# 
-# 
-# )
-
-
-################################################################################
-################################################################################
-# Look/feel
-# html
-# css
-# bslib
-# shiny themes
-
-
-################################################################################
-# html
-
-# customize look using html attributes 
-# ui <- navbarPage(title = 'Patent analytics',
-#                  tabPanel(title = 'Home',
-#                           wellPanel(
-#                             HTML('<p style="font-size:20px;font-weight:bold;background-color: #000;">Some text here</p>')
-#                           )
-#                  ),
-#                  tabPanel(title = 'Charts',
-#                  )
-# 
-# )
-
-
-
-
-################################################################################
-# css
-
-# adding css to html elements directly 
-# ui <- navbarPage(title = 'Patent analytics',
-#                  tabPanel(title = 'Home',
-# 
-#                           wellPanel(style = 'margin-top: 20px; margin-left: 25px; background-color: coral;',
-#                             p(style = 'background-color: #000; font-size: 20px;','Some text here')
-#                             # same as 
-#                             # HTML('<p style="font-size:20px;font-weight:bold;background-color: #000;">Some text here</p>')
-#                           )
-#                  ),
-#                  tabPanel(title = 'Charts',
-#                  )
-# 
-# )
-
-
-# adding a custom css file
-# ui <- navbarPage(title = 'Patent analytics', 
-#                  #theme = 'custom.css',
-#                  tabPanel(title = 'Home',
-#                           
-#                           wellPanel(
-#                             p('Some text here')
-#                           )
-#                  ),
-#                  tabPanel(title = 'Charts',
-#                  )
-#                  
-# )
-
-
-################################################################################
-# bslib (shinythemes library also offers bootswatch themes)
-
-# bootswatch_themes()
-# https://bootswatch.com/
-
 mytheme <- bs_theme(version = 5,
                     bootswatch = 'yeti',
                     bg = '#FFF',
@@ -333,11 +8,7 @@ ui <- navbarPage(title = 'Patent Analytics',
                    tabPanel(title = 'Home',
 
                             wellPanel(
-                              h4('Rett Was Here'),
-                              column(3,
-                                     selectInput("select", h3("Select box"),
-                                     choices = cpc$patent_id)
-                                     )
+                              h4('Patent Analytics')
                             )
                    ),
                    tabPanel(title = 'Competitive Analysis',
@@ -345,64 +16,34 @@ ui <- navbarPage(title = 'Patent Analytics',
                               fluidRow(
                                 HTML('<p style = "padding-left: 30px; font-size: 20px;"> Competitive Positioning </p>
                                       <br>
-                                      <p style = "padding-left: 30px;"> This page analyzes the cop pasldf </p>')
+                                      <p style = "padding-left: 30px;"> This page analyzes the market cpc to find trends </p>')
                               ),
                               hr(),br(),
-                              column(width = 3,
-                                     h4('Inputs'),
-                                     wellPanel(
-                                       selectInput(inputId = 'market_cpcs_input',label = 'Market CPCs',choices = unique_cpc_group, multiple = TRUE),
-                                       #selectInput(inputId = 'submarket_cpcs_input',label = 'Submarket CPCs',choices = unique_cpc_group),
-                                       textAreaInput(inputId = 'submarket_labels_input',label = 'Submarket labels (comma separated'),
-                                       actionButton(inputId = 'generate_competitive_positioning',label = 'Generate',width = '35%')
-                                     )
+                              fluidRow(
+                                column(width = 3,
+                                       h4('Inputs'),
+                                       wellPanel(
+                                         selectInput(inputId = 'market_cpcs_input',label = 'Market CPCs',choices = unique_cpc_group, multiple = TRUE,width = '100%'),
+                                         #selectInput(inputId = 'submarket_cpcs_input',label = 'Submarket CPCs',choices = unique_cpc_group, multiple = TRUE,width = '100%'),
+                                         textAreaInput(inputId = 'submarket_cpcs_input',label = 'Submarket CPCs (comma separated, in quotes)'),
+                                         textAreaInput(inputId = 'submarket_labels_input',label = 'Submarket labels (comma separated)'),
+                                         actionButton(inputId = 'generate_competitive_positioning',label = 'Generate',width = '100%')
+                                       )
+                                ),
+                                column(width = 8, offset = 1,
+                                       h4('Outputs'),
+                                       DTOutput(outputId = 'competition_dt')
+                                ),
                               ),
-                              column(width = 8, offset = 1,
-                                     h4('Outputs'),
-                                     textOutput(outputId = 'text_labels_output'),
-                                     DTOutput(outputId = 'competition_dt')
+                              column(width = 6,
+                                     h4('USA County Graph'),
+                                     plotlyOutput(outputId = 'competition_plot')
                               )
                             )
                    ),
                    tabPanel(title = 'Technology Trends',
-                            h4('Another test...')
+                            h4('Another test...'),
+                            DTOutput(outputId = 'competition_sub_seg')
                             )
   )
 
-
-
-
-################################################################################
-################################################################################
-# Inputs and outputs
-
-# https://shiny.rstudio.com/tutorial/written-tutorial/lesson3/
-# https://fontawesome.com/search?o=r&m=free
-
-# ui <- navbarPage(title = 'Patent analytics',
-#                  theme = 'custom.css',
-#                  tabPanel(title = 'Home',
-#                           column(width = 6,
-#                                  wellPanel(
-#                                    p('Inputs'),
-#                                    selectInput(inputId = 'input1',
-#                                                label = 'Select input',
-#                                                choices = c('Option 1','Option 2','Option 3'),
-#                                                multiple = T,
-#                                                width = '200px'),
-#                                    actionButton(inputId = 'button1',label = 'Github',icon = icon('github')
-#                                                 )
-#                                  )
-#                                  ),
-#                           column(width = 6,
-#                                  wellPanel(
-#                                    p('Outputs'),
-#                                    plotlyOutput(outputId = 'chart1')
-#                                  )
-#                                  )
-# 
-#                  ),
-#                  tabPanel(title = 'Charts',
-#                  )
-# 
-# )
